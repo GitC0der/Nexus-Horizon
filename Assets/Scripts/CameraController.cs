8da5c002697fe07884a73900ee69d3b1c24890b1
Danny <danny.seel@epfl.ch>
2023-04-20 12:22:15 +0200

    Camera updated and optimised

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,35 +3,28 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private float moveSpeed = 10.0f;
-    private float rotationSpeed = 360.0f;
     private float rotationSpeedLR = 60f;
     private float rotationSpeedUD = 60f;
     
     void Update()
     {
         // Camera horizontal and vertical movement
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("HorizontalWASD");
+        float vertical = Input.GetAxis("VerticalWASD");
         float updown = Input.GetAxis("UpDown");
 
         transform.Translate(new Vector3(horizontal, updown, vertical) * moveSpeed * Time.deltaTime);
 
 		// Rotational movement
-        if (Input.GetKey(KeyCode.Q)) 
-        {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
             transform.Rotate(Vector3.up, -rotationSpeedLR * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.E))
-        {
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
             transform.Rotate(Vector3.up, rotationSpeedLR * Time.deltaTime);
         }
         
-        if (Input.GetKey(KeyCode.X))
-        {
+        if (Input.GetKey(KeyCode.UpArrow)) {
             transform.Rotate(Vector3.right, -rotationSpeedUD * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.Y))
-        {
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
             transform.Rotate(Vector3.right, rotationSpeedUD * Time.deltaTime);
         }
 
