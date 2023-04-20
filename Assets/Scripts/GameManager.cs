41e03fe6106ca174f84783158a2766552b83df70
Amerilla <alain.merillat@epfl.ch>
2023-04-20 10:54:30 +0200

    Generates cube of cube

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,37 +6,51 @@ public class GameManager : MonoBehaviour {
     public GameObject cubePrefab;  // Prefab of the cube to be spawned
     public int numCubesPerLine = 10;      // Number of cubes to spawn per line
     public int numLines = 10;      // Number of lines to spawn
-    private float spawnInterval = 0.3f;  // Time between each cube spawn
+    private float spawnInterval = 0.1f;  // Time between each cube spawn
 
     private float timer = 0.0f;     // Timer to track time between spawns
-    private int cubesSpawned = 0;   // Number of cubes spawned so far
-    private int linesSpawned = 0;   // Number of lines spawned so far
+    private int widthX = 10;
+    private int widthY = 10;
+    private int widthZ = 10;
+
+    private int x = 0;
+    private int y = 0;
+    private int z = 0;
+        
 
     // Use this for initialization
     void Start () {
-        InvokeRepeating("SpawnCube", 0.0f, spawnInterval);
     }
     
     // Update is called once per frame
     void Update () {
+        timer += Time.deltaTime;
         
+        if (timer >= spawnInterval)
+        {
+            timer = 0;
+            SpawnCube(transform.position + x*Vector3.right + y*Vector3.forward + z*Vector3.up);
+            ++x;
+            if (x >= widthX) {
+                x = 0;
+                ++y;
+            }
+
+            if (y >= widthY) {
+                y = 0;
+                ++z;
+            }
+
+            if (z > widthZ) {
+                z = 0;
+                timer = -100000;
+            }
+        }
     }
 
     // Spawn a single cube
-    void SpawnCube() {
+    void SpawnCube(Vector3 position) {
         // Spawn a new cube
-        Instantiate(cubePrefab, transform.position + Vector3.right * cubesSpawned + Vector3.forward * linesSpawned, Quaternion.identity);
-        cubesSpawned++;
-        // If we've spawned all the cubes in the current line
-        if (cubesSpawned >= numCubesPerLine) {
-            // Reset the cube counter
-            cubesSpawned = 0;
-            linesSpawned++;
-            // If we've spawned all the lines
-            if (linesSpawned >= numLines) {
-                // Cancel the repeating invocation of the SpawnCube method
-                CancelInvoke("SpawnCube");
-            }
-        }
+        Instantiate(cubePrefab, position, Quaternion.identity);
     }
 }
