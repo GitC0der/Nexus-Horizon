bc53501acdaf20bef86125fba4c0894a547f35e6
Danny <danny.seel@epfl.ch>
2023-04-20 13:05:29 +0200

    Categories now spawn based on a static probability

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,82 @@
+// using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+//
+// public class GameManager : MonoBehaviour {
+//
+//     public GameObject buildingPrefab;
+//     public GameObject parkPrefab;
+//     public GameObject trainPrefab;
+//     public GameObject skybridgePrefab;
+//     public GameObject walkwayPrefab;
+//     
+//     private GameObject[] prefabs;
+//     private Dictionary<string, float> map = new Dictionary<string, float>()
+//     {
+//         { "building", 0.4f },
+//         { "park", 0.15f },
+//         { "train", 0.15f },
+//         { "skybridge", 0.1f },
+//         { "walkway", 0.2f }
+//     };
+//
+//     private int nbPrefabs = 5;
+//
+//     private float spawnInterval = 0.1f;  // Time between each cube spawn
+//
+//     private float timer = 0.0f;     // Timer to track time between spawns
+//     private int widthX = 3;
+//     private int widthY = 3;
+//     private int widthZ = 3;
+//
+//     private int x = 0;
+//     private int y = 0;
+//     private int z = 0;
+//
+//     // Use this for initialisation
+//     void Start () {
+//         // Initialise the prefabs array with the given gameObjects
+//         prefabs = new GameObject[] { buildingPrefab, parkPrefab, trainPrefab, skybridgePrefab, walkwayPrefab };
+//     }
+//     
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         timer += Time.deltaTime;
+//
+//         if (timer >= spawnInterval)
+//         {
+//             timer = 0;
+//             SpawnCube(transform.position + x * Vector3.right + y * Vector3.forward + z * Vector3.up);
+//             ++x;
+//             if (x >= widthX) {
+//                 x = 0;
+//                 ++y;
+//             }
+//
+//             if (y >= widthY) {
+//                 y = 0;
+//                 ++z;
+//             }
+//
+//             if (z >= widthZ) {
+//                 z = 0;
+//                 timer = -100000;
+//             }
+//         }
+//     }
+//
+//     // Spawn a single cube
+//     void SpawnCube(Vector3 position) {
+//         int rand = Random.Range(0, nbPrefabs);
+//         // Spawn a new cube using a randomly selected prefab from the prefabs array
+//         Instantiate(prefabs[rand], position, Quaternion.identity);
+//     }
+// }
+
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
@@ -8,27 +85,31 @@ public class GameManager : MonoBehaviour {
     public GameObject trainPrefab;
     public GameObject skybridgePrefab;
     public GameObject walkwayPrefab;
-    
-    private GameObject[] prefabs;
-    private int nbPrefabs = 5;
+
+    private List<GameObject> prefabs;
+    private Dictionary<string, float> map = new Dictionary<string, float>()
+    {
+        { "Building", 0.4f },
+        { "Park", 0.15f },
+        { "Train", 0.15f },
+        { "SkyBridge", 0.1f },
+        { "Walkway", 0.2f }
+    };
 
     private float spawnInterval = 0.1f;  // Time between each cube spawn
-
     private float timer = 0.0f;     // Timer to track time between spawns
     private int widthX = 3;
     private int widthY = 3;
     private int widthZ = 3;
-
     private int x = 0;
     private int y = 0;
     private int z = 0;
 
-    // Use this for initialization
+    // Use this for initialisation
     void Start () {
-        // Initiasize the prefabs array with the five given gameObjects
-        prefabs = new GameObject[] { buildingPrefab, parkPrefab, trainPrefab, skybridgePrefab, walkwayPrefab };
+        prefabs = new List<GameObject>() { buildingPrefab, parkPrefab, trainPrefab, skybridgePrefab, walkwayPrefab };
     }
-    
+
     // Update is called once per frame
     void Update()
     {
@@ -37,13 +118,12 @@ public class GameManager : MonoBehaviour {
         if (timer >= spawnInterval)
         {
             timer = 0;
-            SpawnCube(transform.position + x * Vector3.right + y * Vector3.forward + z * Vector3.up);
+            SpawnPrefab(transform.position + x * Vector3.right + y * Vector3.forward + z * Vector3.up);
             ++x;
             if (x >= widthX) {
                 x = 0;
                 ++y;
             }
-
 
             if (y >= widthY) {
                 y = 0;
@@ -57,10 +137,25 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    // Spawn a single cube
-    void SpawnCube(Vector3 position) {
-        int rand = Random.Range(0, nbPrefabs);
-        // Spawn a new cube using a randomly selected prefab from the prefabs array
-        Instantiate(prefabs[rand], position, Quaternion.identity);
+    void SpawnPrefab(Vector3 position)
+    {
+        float totalProbability = 0.0f;
+        foreach (var item in map)
+        {
+            totalProbability += item.Value;
+        }
+
+        float randomValue = Random.Range(0.0f, totalProbability);
+
+        float cumulativeProbability = 0.0f;
+        foreach (var item in map)
+        {
+            cumulativeProbability += item.Value;
+            if (randomValue < cumulativeProbability)
+            {
+                Instantiate(prefabs.Find(p => p.name.Contains(item.Key)), position, Quaternion.identity);
+                break;
+            }
+        }
     }
 }
